be3aefba09c6b68b86f1eaefe9b47107f1e2903f
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-10-16 01:51:46 +0600

    nav header done

--- src/componants/Header.js
+++ src/componants/Header.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>
-            <h2>This is Header</h2>
+            <div className="navbar bg-primary text-primary-content">
+                <Link className="btn btn-ghost normal-case text-xl">Awesome Auth</Link>
+                <Link className="btn btn-ghost normal-case text-xl" to='/'>Home</Link>
+                <Link className="btn btn-ghost normal-case text-xl" to='/login'>Login</Link>
+                <Link className="btn btn-ghost normal-case text-xl" to='/register'>Register</Link>
+            </div>
         </div>
     );
 };
