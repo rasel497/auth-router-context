45918ace05b867a360ec4ac95d4653c941a58006
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-10-16 16:58:53 +0600

    Access Context Value from anywhere inside the app

---
+++ src/__steps.js
@@ -0,0 +1,23 @@
+/*
+M-56-4:: BASIC Context API Setup!
+-----------------------------------
+1. context API: share auth state withother componants(accrose the application)
+Meaning: auth state mean user login kora ase ki nei thakle kon user ta login kora ase ata ami sobar sthe share korte chai.
+2. Without Prop drill jekono jayag thke access korte pari.  Eii jonno amra:: 
+Create an UserContex &
+ContextProvider (with passed the children) use korsi.
+
+3. set the UserContext in the src> index.js file
+4. To consume the context: export the AuthContext from UserContext
+5. Now at Header or anywhere else: use useContext hook
+6. Now at Header or Home () or anywhere else): use useContext hook to get the info in the context.
+*/
+
+/*
+Now, Auth Integration Go  to Firebase
+-----------------------------------------
+1.  use getAuth by passing the app from firebase config
+2.
+3.
+
+*/
--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,10 +1,17 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
+import { getAuth } from 'firebase/auth';
+import app from '../firebase/firebase.config';
 
-const AuthContext = createContext();
+
+export const AuthContext = createContext();
+
+const auth = getAuth(app)
 
 const UserContext = ({ children }) => {
+    // const user = { displayName: 'Akash' }
+    const [user, setUser] = useState({ displayName: 'AAAKashhh' })
 
-    const authInfo = {};
+    const authInfo = { user: user };
 
     return (
         <AuthContext.Provider value={authInfo}>
